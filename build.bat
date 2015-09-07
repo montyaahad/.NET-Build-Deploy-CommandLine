97451ba17e93c9f559525d17aebf9149d91e3028
montyaahad <montyaahad@gmail.com>
2015-09-07 23:02:49 +0600

    build and deploy asp.net project

---
+++ build.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE
+devenv.exe "E:\ASP.NET\WebApplication1\WebApplication1.sln" /rebuild DEBUG
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+MSBuild E:\ASP.NET\WebApplication1\WebApplication1\WebApplication1.csproj /p:DeployOnBuild=true /p:PublishProfile=Test /p:VisualStudioVersion=12.0
